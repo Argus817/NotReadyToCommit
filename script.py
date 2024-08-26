b12e21650c9dfe48a49cb64cb470e64a7091f8fe
Argus817 <namantandel@gmail.com>
2024-08-26 19:37:10 +0530

    Initial Commit

---
+++ script.py
@@ -0,0 +1,25 @@
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad,unpad
+import os
+
+KEY = b'??????'
+IV = os.urandom(16)
+
+assert len(KEY) == 16
+assert len(IV) == 16
+
+def encrypt(plaintext, key, iv):
+    padded = pad(plaintext, 16)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    ciphertext = cipher.encrypt(padded)
+    return iv+ciphertext
+
+def main():
+    with open('location.png','rb') as file:
+        data = file.read()
+    encData = encrypt(data, KEY, IV)
+    with open('encrypted.png', 'wb') as encFile: 
+        encFile.write(encData)
+
+if __name__ == "__main__":
+    main()
